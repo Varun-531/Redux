28f292c53fe5cb092c5eba42573768eb9381d413
Varun-531 <126848991+Varun-531@users.noreply.github.com>
2025-03-02 11:39:41 +0530

    Add todoSlice (#2)
    * Add todoSlice
    
    * Added RemoveAll
    
    ---------
    
    Co-authored-by: ChVarunFE <chennaparapu.varun@razorpay.com>

--- src/features/todo/todoSlice.ts
+++ src/features/todo/todoSlice.ts
@@ -1,1 +1,34 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: "1", title: "Learn Redux" }],
+};
+type Todo = {
+  id: string;
+  title: string;
+};
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo: Todo = { id: nanoid(), title: action.payload };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      const todo = state.todos.find((todo) => todo.id == action.payload.id);
+      if (todo) {
+        todo.title = action.payload.title;
+      }
+    },
+    removeAll: (state) => {
+      state.todos = [];
+    },
+  },
+});
+
+export const { addTodo, removeTodo, updateTodo, removeAll } = todoSlice.actions;
+export default todoSlice.reducer;
